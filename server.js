788e806f2d69f40f35a8abbe9e9447c84483a852
Kim Laï Trinh <kimlai@lrqdo.fr>
2015-09-26 15:35:25 +0200

    [api] Add sharing announce by email.

--- server.js
+++ server.js
@@ -6,8 +6,12 @@ var serve = require('koa-static');
 var bodyParser = require('koa-bodyparser');
 var knexfile = require('./knexfile');
 var knex = require('knex')(knexfile);
-var _ = require('lodash');
 var uuid = require('uuid');
+var mailgun = require('mailgun-js')({
+    apiKey: 'key-06932b00494fc1a8f0949d56e266a6fe',
+    domain: 'sandbox85bfb808d09e4e84aed1eef80802a2fc.mailgun.org'
+});
+var _ = require('underscore');
 
 var app = koa();
 
@@ -26,6 +30,7 @@ router.get('/', index);
 router.post('/api/accounts', createAccount);
 router.post('/api/announces', createAnnounce);
 router.get('/api/announces', listAnnounces);
+router.post('/api/share-announce', shareAnnounce);
 app.use(router.routes());
 
 function *index() {
@@ -58,6 +63,30 @@ function *listAnnounces() {
         .from('announces')
         .orderBy('createdat', 'DESC')
     this.body = announces;
+}
+
+function *shareAnnounce() {
+    var announceUuid = this.request.body.announceUuid;
+    var emails = this.request.body.emails;
+    var message = this.request.body.message;
+    var url = process.env.APP_URL + '/annonce/' + announceUuid;
+    var compiledTemplate = _.template(
+        "Bonjour,\n\nUne announce Peligourmet viens d'être paratgée avec vous.\n\nVoici le message de l'annonceur:\n\n-----------------------------------------------------------------------\n<%= message %>\n-----------------------------------------------------------------------\n\nPour voir l'annonce et reserver, visitez <%= url%>"
+    );
+    var email = {
+        from: 'Peligourmet <peligourmetparis@gmail.com>',
+        to: emails.join(','),
+        subject: 'Nouvelle annonce',
+        text: compiledTemplate({ message: message, url: url }),
+    };
+    mailgun.messages().send(email, function (error, info) {
+        if (error) {
+            return console.log(error);
+        }
+    });
+
+    this.status = 201;
+    this.body = "OK";
 }
 
 app.listen(process.env.PORT || 3000);
