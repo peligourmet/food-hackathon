80b6cf2a0919f3e67e6b49b8aa3a3a14b195cdaa
Erik Escoffier <hello@nerik.me>
2015-09-26 17:10:16 +0200

    Fixed bug

--- components/ShowAnnonce.react.js
+++ components/ShowAnnonce.react.js
@@ -6,11 +6,11 @@ var reactor = require('../reactor')
 module.exports = React.createClass({
 
     render: function () {
-        var annonce = reactor.evaluate(getters.getAnnonceById(this.props.params.id));
+        var annonce = reactor.evaluate(getters.getAnnonceById(this.props.params.uuid));
         return (
             <div>
                 <div className="pageHeader">
-                    {annonce.get('name')}
+                    {annonce.get('product_type')}
                     <p className="header-details">A récupérer à <b>Bordeaux</b> le <b>lundi 12 septembre</b></p>
                 </div>
                 <div className="main">
