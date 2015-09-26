cf4c2bd5d03f86da3505526a1f4e9929011c5f49
Erik Escoffier <hello@nerik.me>
2015-09-26 19:30:28 +0200

    Added a basic homepage

--- components/Home.react.js
+++ components/Home.react.js
@@ -5,7 +5,14 @@ module.exports = React.createClass({
 
     render: function () {
         return (
-            <div>Home</div>
+            <div className="homePanel">
+                <div className="homePanel-text">
+                    Etiam euismod malesuada cursus. Suspendisse vel tincidunt quam, porta sollicitudin enim.
+                </div>
+                <div className="homePanel-cta">
+                    <Link to="annonces/create">Je suis un pelicab</Link>
+                </div>
+            </div>
         );
     }
 });
