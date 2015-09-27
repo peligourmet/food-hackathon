842f867f0540b1b8c01ac78f7c193b66f2a39983
Erik Escoffier <hello@nerik.me>
2015-09-27 10:30:19 +0200

    Styling the product page

--- components/ShowAnnonce.react.js
+++ components/ShowAnnonce.react.js
@@ -17,28 +17,27 @@ module.exports = React.createClass({
                     <div className="annonceDescription">
                         <p>"{annonce.get('description')}"</p>
                         <p>{annonce.get('pelicabname')}</p>
-                    <div className="quantityBox">
-                        <div>Quantité disponible</div>
-                        <span className="quantity">{annonce.get('quantity')}</span>
-                        <span className="quantity-unit">{annonce.get('quantityunit')}</span>
-                    </div>
                     </div>
 
-
-                    <p className="orderSummary">Jean-Baptiste C. a réservé 2 kilos</p>
-                    <p className="orderSummary">Kim Laï T. a réservé 4 kilos</p>
-
-
-                    <div className="buyBox">
-                        <select>
-                            <option>1 kg</option>
-                            <option>2 kg</option>
-                            <option>3 kg</option>
-                            <option>4 kg</option>
-                            <option>5 kg</option>
-                        </select>
-                        <button>Réserver</button>
+                    <div className="col">
+                        <p className="orderSummary">Jean-Baptiste C. a réservé 2 {annonce.get('quantityunit')}</p>
+                        <p className="orderSummary">Kim Laï T. a réservé 4 {annonce.get('quantityunit')}</p>
                     </div>
+
+                    <div className="col">
+                        <p className="availability">Encore {annonce.get('quantity')} {annonce.get('quantityunit')} disponibles</p>
+                        <div className="buyBox">
+                            <select>
+                                <option>1 kg</option>
+                                <option>2 kg</option>
+                                <option>3 kg</option>
+                                <option>4 kg</option>
+                                <option>5 kg</option>
+                            </select>
+                            <button>Réserver</button>
+                        </div>
+                    </div>
+
                 </div>
             </div>
         );
